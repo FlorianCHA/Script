b812868a0bc6997ff8ac3244c8dfd2ee5369f009
FlorianCHA <florian.charriat@gmail.com>
2018-04-09 14:54:10 +0200

    Mise a jour de la doc

--- epydoc.sh
+++ epydoc.sh
@@ -1,4 +1,4 @@
-epydoc --html ./Cluster/*.py -o ./docs/ -v --graph all --inheritance grouped --docformat="restructuredtext" -n 'Script Flo' --navlink='<a href="https://floriancha.github.io/stage/">Retour au Site<\a>'
+epydoc --html ./Cluster/*.py -o ./docs/ -v --graph all --inheritance grouped --docformat="restructuredtext" -n 'Script Flo' --navlink='<a href="https://floriancha.github.io/stage/" target="_blank">Retour au Site</a>'
 git add *
 git commit -m 'Mise a jour de la doc'
 git push
