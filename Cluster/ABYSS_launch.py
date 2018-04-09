a7d44ab0bc9208392770a033da4b63fe16056621
FlorianCHA <florian.charriat@gmail.com>
2018-04-09 14:25:03 +0200

    Mise a jour de la doc

--- Cluster/ABYSS_launch.py
+++ Cluster/ABYSS_launch.py
@@ -51,7 +51,9 @@ from module_Flo import verifDir, createDir , form
 
 if __name__ == "__main__":
 
-	version = '0.1'
+############### Variables Globales #############################""""
+	##Variables Globales
+	version="0.1" 
 	
 ############ Argparse #####################
 	parser = argparse.ArgumentParser(prog=__file__, description='''This program is used to assemble all fasta files in a directory using the ABYSS tool. The assembly will be done with different lengths of kmère (20, 30, 40, 50, 60, 70, 80 and 90) ''')
