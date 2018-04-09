5e5998d7337c0070c90550d3977fa5bd65d27857
FlorianCHA <florian.charriat@gmail.com>
2018-04-09 14:49:26 +0200

    Mise a jour de la doc

--- epydoc.sh
+++ epydoc.sh
@@ -1,4 +1,4 @@
-epydoc --html ./Cluster/*.py -o ./docs/ -v --graph all --inheritance grouped --docformat="restructuredtext" -N Script
+epydoc --html ./Cluster/*.py -o ./docs/ -v --graph all --inheritance grouped --docformat="restructuredtext" -n 'Script Flo'
 git add *
 git commit -m 'Mise a jour de la doc'
 git push
