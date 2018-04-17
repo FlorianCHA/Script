7062a70c63c79980e2473d0b2330e83ee93187ee
FlorianCHA <florian.charriat@gmail.com>
2018-04-17 13:42:22 +0200

    Mise a jour de la doc

--- Cluster/Test_module.py
+++ Cluster/Test_module.py
@@ -1,8 +1,9 @@
 from module_Flo import directory
 
-directory = directory("/home/charriat/Documents")
+directory = directory("/home/charriat/Documents/Script")
 print(directory.path)
 print(directory.listAll)
 print(directory)
 print(directory.listExt('directory'))
-
+print(directory.listExt('fasta'))
+print(directory.listExt('allfastq'))
