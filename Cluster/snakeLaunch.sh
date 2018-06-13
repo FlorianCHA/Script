bd6d359e1bdf3ff8691259d62a150859dc293ed3
florianCHA <florian.charriat@gmail.com>
2018-06-13 09:59:09 +0200

    d

--- Cluster/snakeLaunch.sh
+++
@@ -1,3 +0,0 @@
-module unload system/python/2.7.9
-module load bioinfo/snakemake/3.13.3
-snakemake -s BRAKER_pipeline.snake --jobs 50 --cluster "qsub -q long.q -l mem_free=30G -cwd -V -pe parallel_smp {threads}"
